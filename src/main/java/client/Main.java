c0e8d35a542ceff88a7f92584b0e33e242d2bfe2
nursabir <fikus710828@gmail.com>
2023-03-19 22:14:54 +0300

    Есть все фичи, не работает подключение к нескольким клиентам

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -12,19 +12,22 @@ public class Main {
 
     public static void main(String[] args) {
         // todo - your java-code
+        launch();
+    }
+
+    private static void launch() {
         try {
             Socket socket = new Socket(SERVER_ADDRESS, PORT);
             inputStream = new DataInputStream(socket.getInputStream());
             outputStream = new DataOutputStream(socket.getOutputStream());
             Scanner scanner = new Scanner(System.in);
-            //System.out.println("Выберите действие, которое хотите произвести --> 1-отрпавить файл на сервер\n 2-достать файл из сервера \n 3-удалить файл из сервера");
-            //int action = scanner.nextInt();
-            sendFile("C:\\Users\\fikus\\IdeaProjects\\se-lab04-tmp2223\\src\\main\\java\\client\\data\\1.txt");
+            System.out.println("Enter action (1 - get a file, 2 - save a file, 3 - delete a file)");
+            int action = scanner.nextInt();
+            outputStream.writeUTF(String.valueOf(action));
+            doAction(action);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 
     private static void sendFile(String fileName) throws IOException {
@@ -36,11 +39,102 @@ public class Main {
         byte[] buffer = new byte[(int) file.length()];
         // в переменнную буфер считываем все байты из файла
         fileInputStream.read(buffer);
-        for(int i =0; i<buffer.length; i++) {
+        for (int i = 0; i < buffer.length; i++) {
             System.out.println(buffer[i]);
         }
         // отправили через сокет
         outputStream.write(buffer);
+    }
+
+    private static void doAction(int action) throws IOException {
+        switch (action) {
+            case (1): {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Do you want to get the file by name or by id (1 - name, 2 - id):");
+                String recieveAction = sc.nextLine();
+                if (recieveAction.equals("1")) {
+                    System.out.println("Enter name of file");
+                    Scanner sc1 = new Scanner(System.in);
+                    String a = sc1.nextLine();
+                    outputStream.writeUTF(a);
+                    writeBytesInNewFile(a, getArrayOfBytesFromSocketStream());
+                    System.out.println(inputStream.readUTF());
+                    // получаем по имени
+                } else if (recieveAction.equals("2")) {
+                    // получаем по id
+                    System.out.println("Enter id of file");
+                    Scanner sc1 = new Scanner(System.in);
+                    String a = sc1.nextLine();
+                    outputStream.writeUTF(a);
+                    //  String name = "C:\\Users\\fikus\\IdeaProjects\\se-lab04-tmp2223\\src\\main\\java\\client\\data\\" + a;
+
+                    writeBytesInNewFile(inputStream.readUTF(), getArrayOfBytesFromSocketStream());
+                    System.out.println(inputStream.readUTF());
+                }
+                // получили файл
+            }
+            case (2): {
+                System.out.println("Enter name of the file:");
+                Scanner sc = new Scanner(System.in);
+                String nameOfFile = sc.nextLine();
+                outputStream.writeUTF(nameOfFile);
+                String a = inputStream.readUTF();
+                if (a.equals("File already exist")) {
+                    System.out.println("File already exist in server");
+                    break;
+                } else {
+                    System.out.println(a);
+
+                    try {
+                        sendFile("C:\\Users\\fikus\\IdeaProjects\\se-lab04-tmp2223\\src\\main\\java\\client\\data\\" + nameOfFile);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Unique id for " + nameOfFile + " " + inputStream.readUTF());
+                    break;
+                }
+            }
+            case (3): {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Do you want to delete the file by name or by id (1 - name, 2 - id):");
+                String deleteAction = sc.nextLine();
+                if (deleteAction.equals("1")) {
+                    System.out.println("Enter name of file");
+                    Scanner sc1 = new Scanner(System.in);
+                    String a = sc1.nextLine();
+                    outputStream.writeUTF(a);
+                    System.out.println(inputStream.readUTF());
+                    // удаляем по имени
+                } else if (deleteAction.equals("2")) {
+                    // удаляем по id
+                    System.out.println("Enter id of file");
+                    Scanner sc1 = new Scanner(System.in);
+                    String a = sc1.nextLine();
+                    outputStream.writeUTF(a);
+                    System.out.println(inputStream.readUTF());
+                }
+                // удалили файл из сервера
+            }
+        }
+    }
+
+    private static byte[] getArrayOfBytesFromSocketStream() throws IOException {
+        int length = (int) inputStream.readLong();
+        byte[] resultBytes = new byte[length];
+        inputStream.readFully(resultBytes, 0, length);
+        return resultBytes;
+    }
+
+    // по заданному имени создает файл в data и байты из массива записывает в новый файл
+    private static void writeBytesInNewFile(String nameOfFile, byte[] bytes) throws FileNotFoundException {
+        String pathToNewFile = "C:\\Users\\fikus\\IdeaProjects\\se-lab04-tmp2223\\src\\main\\java\\client\\data\\" + nameOfFile;
+        File file = new File(pathToNewFile);
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        try {
+            fileOutputStream.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/main/java/server/Session.java
+++ src/main/java/server/Session.java
@@ -2,21 +2,29 @@ package server;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.*;
 
 public class Session extends Thread {
     //
     private final Socket socket;
     static DataOutputStream outputStream;
     static DataInputStream inputStream;
+    static Map<String, String> idAndNameOfFile;
 
     public Session(Socket socket) throws IOException {
         this.socket = socket;
         inputStream = new DataInputStream(socket.getInputStream());
         outputStream = new DataOutputStream(socket.getOutputStream());
+        idAndNameOfFile = getNameAndId();
     }
 
     public void run() {
-
+        try {
+            int action = Integer.parseInt(inputStream.readUTF());
+            doAction(action);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     // из соекетового потока получаем массив байтов
@@ -37,6 +45,167 @@ public class Session extends Thread {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void doAction(int action) throws IOException {
+        switch (action) {
+            case (1): {
+                String idOrName = inputStream.readUTF();
+                if (isNumeric(idOrName)) {
+                    if (idAndNameOfFile.containsValue(idOrName)) {
+                        File file = new File("C:\\Users\\fikus\\IdeaProjects\\se-lab04-tmp2223\\src\\main\\java\\server\\data\\" + getKey(idAndNameOfFile, idOrName));
+                        outputStream.writeUTF(getKey(idAndNameOfFile, idOrName));
+                        sendFile(file.getPath());
+                    }
+                } else {
+                    File file = new File("C:\\Users\\fikus\\IdeaProjects\\se-lab04-tmp2223\\src\\main\\java\\server\\data\\" + idOrName);
+                    sendFile(file.getPath());
+                }
+              break;
+
+                // по сокету возвращаем файл
+            }
+            case (2): {
+                String name = inputStream.readUTF();
+                if (idAndNameOfFile.containsKey(name)) {
+                    outputStream.writeUTF("File already exist");
+                    break;
+                } else {
+                    outputStream.writeUTF("File will send");
+                    writeBytesInNewFile(name, getArrayOfBytesFromSocketStream());
+                    // сохранили idшник
+                    String id = generateUniqueValueOfFile();
+                    outputStream.writeUTF(id);
+                    addInDataBasedNewId(name, id);
+                    updateDataBased();
+                }
+            }
+            case (3): {
+                String idOrName = inputStream.readUTF();
+                if (isNumeric(idOrName)) {
+                    if (idAndNameOfFile.containsValue(idOrName)) {
+                        File file = new File("C:\\Users\\fikus\\IdeaProjects\\se-lab04-tmp2223\\src\\main\\java\\server\\data\\" + getKey(idAndNameOfFile, idOrName));
+                        if (file.delete()) {
+                           idAndNameOfFile = rewriteDataBase(idOrName);
+                            updateDataBased();
+                            outputStream.writeUTF("File of ID " + idOrName + " were successful delet");
+                        } else {
+                            outputStream.writeUTF("File of ID " + idOrName + " dont were successful delet");
+                        }
+                    }
+                } else {
+                    File file = new File("C:\\Users\\fikus\\IdeaProjects\\se-lab04-tmp2223\\src\\main\\java\\server\\data\\" + idOrName);
+                    if (file.delete()) {
+                        idAndNameOfFile = rewriteDataBase(idOrName);
+                        updateDataBased();
+                        outputStream.writeUTF("File of name " + idOrName + " were successful delet");
+                    } else {
+                        outputStream.writeUTF("File of name " + idOrName + "dont were successful delet");
+                    }
+                }
+
+                // удаляем переданный файл
+            }
+        }
+    }
+
+    // новый уникальный id
+    private static String generateUniqueValueOfFile() {
+        boolean found = false;
+        String candidate = null;
+        while (!found) {
+            candidate = String.valueOf((int) (Math.random() * 100));
+            if (!idAndNameOfFile.containsValue(candidate)) {
+                found = true;
+            }
+        }
+        return candidate;
+    }
+
+    private static void addInDataBasedNewId(String nameOfFile, String id) {
+        idAndNameOfFile.put(nameOfFile, id);
+    }
+
+    // записываем текущюу мапу в базу данных
+    private static void updateDataBased() throws IOException {
+        FileWriter dataBased = null;
+        try {
+            dataBased = new FileWriter("C:\\Users\\fikus\\IdeaProjects\\se-lab04-tmp2223\\src\\main\\java\\server\\data\\bd.txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (Map.Entry<String, String> entry : idAndNameOfFile.entrySet()) {
+            System.out.println(entry.getKey() + entry.getValue());
+            dataBased.write(entry.getKey() + " " + entry.getValue() + "\n");
+        }
+        dataBased.close();
+        // каждый раз обновляем
+        idAndNameOfFile = getNameAndId();
+    }
+
+    // вовзращение мапа из бд - ключи имена значения ид
+    public static Map<String, String> getNameAndId() {
+        Map result = new HashMap();
+        File file = new File("C:\\Users\\fikus\\IdeaProjects\\se-lab04-tmp2223\\src\\main\\java\\server\\data\\bd.txt");
+        Scanner sc;
+        try {
+            sc = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        while (sc.hasNextLine()) {
+            String str = sc.nextLine();
+            result.put(str.split(" ")[0], str.split(" ")[1]);
+        }
+        return result;
+    }
+
+    // чисто для проверки число ли строка
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    // чтобы достать ключ по значению
+    public static <K, V> K getKey(Map<K, V> map, V value) {
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            if (value.equals(entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    private static void sendFile(String fileName) throws IOException {
+        File file = new File(fileName);
+        FileInputStream fileInputStream = new FileInputStream(file);
+        // отправляем через сокет длину файла
+        outputStream.writeLong(file.length());
+        // тут массив может быть очень большой, необходимо будет это учитывать потом
+        byte[] buffer = new byte[(int) file.length()];
+        // в переменнную буфер считываем все байты из файла
+        fileInputStream.read(buffer);
+        for (int i = 0; i < buffer.length; i++) {
+            System.out.println(buffer[i]);
+        }
+        // отправили через сокет
+        outputStream.write(buffer);
+    }
+    public static Map<String, String> rewriteDataBase(String nameOrId){
+        Map<String, String> map = new HashMap<>();
+        List<String> keys = new ArrayList<String>(idAndNameOfFile.keySet());
+        for(int i = 0; i < keys.size(); i++) {
+            String key = keys.get(i);
+            String value = idAndNameOfFile.get(key);
+            if( ! (key.equals(nameOrId) || value.equals(nameOrId))){
+                map.put(key, value);
+            }
+        }
+        return map;
     }
 
 }
